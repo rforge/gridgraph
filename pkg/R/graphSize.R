5a8bdba86a1ca801aed91792bb5b57f25eee398f
anhinton <anhinton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-22 01:57:24 +0000

    added documentation for agopenSized()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gridgraph@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/graphSize.R
+++ pkg/R/graphSize.R
@@ -22,7 +22,9 @@ graphSize <- function (Ragraph) {
 ## agopenSized produces an Ragraph object of the size determined by graphviz
 agopenSized <- function(graph, name, nodes, edges, kind = NULL,
                         layoutType = "dot", attrs = list(), nodeAttrs = list(), 
-                        edgeAttrs = list(), subGList = list()) {
+                        edgeAttrs = list(), subGList = list(),
+                        edgeMode = edgemode(graph),
+                        recipEdges = c("combined", "distinct")) {
     # set size to 0x0 inches to let graphviz set size of boundBox
     attrs$graph$size <- "0,0"
     # set edge weights to those specified in graph (Rgraphviz agopen() does not
@@ -33,11 +35,13 @@ agopenSized <- function(graph, name, nodes, edges, kind = NULL,
     # create temporary Ragraph to extract boundBox info
     temp <- agopen(graph=graph, name=name, nodes=nodes, edges=edges, kind=kind, 
                    layout=TRUE, layoutType=layoutType, attrs=attrs, 
-                   nodeAttrs=nodeAttrs, edgeAttrs=edgeAttrs, subGList=subGList)
+                   nodeAttrs=nodeAttrs, edgeAttrs=edgeAttrs, subGList=subGList,
+                   edgeMode=edgeMode, recipEdges=recipEdges)
     # set graph size attribute with size generated in 'temp'
     attrs$graph$size <- graphSize(temp)
     
     agopen(graph=graph, name=name, nodes=nodes, edges=edges, kind=kind, 
            layout=TRUE, layoutType=layoutType, attrs=attrs, 
-           nodeAttrs=nodeAttrs, edgeAttrs=edgeAttrs, subGList=subGList)
+           nodeAttrs=nodeAttrs, edgeAttrs=edgeAttrs, subGList=subGList,
+           edgeMode=edgeMode, recipEdges=recipEdges)
 }
