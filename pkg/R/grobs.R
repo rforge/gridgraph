719d6189e6f4ce71a24848b523d529466ab5ce65
anhinton <anhinton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-12 23:25:30 +0000

    moved makeArrow() to grobs.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gridgraph@47 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/grobs.R
+++ pkg/R/grobs.R
@@ -70,3 +70,75 @@ nodeBox <- function(shape, name, x, y, height, lwidth, rwidth, color,
     }
     box
 }
+
+makeArrow <- function(arrowType, arrowsize, startX, startY, endX, endY, 
+                      col, lwd, lty, name) {
+  if (arrowType == "normal") arrowType <- "closed"
+  if (arrowType == "vee") arrowType <- "open"
+  if (arrowType == "none" || arrowType == "open" || arrowType == "closed") {
+    arrow <- NULL
+    if (arrowType != "none") {
+      # FIXME: using a calculated 'length' does not scale when adjusted by
+      # arrowsize, so graphviz default length 10 used
+      arrowlen <- unit(arrowsize*10, "native")
+      arrow <- arrow(angle=20, type=arrowType, length=arrowlen)
+    }
+    z <- segmentsGrob(startX, startY,
+                      endX, endY,
+                      default.units="native",
+                      arrow=arrow, name=name,
+                      gp=gpar(col=col, fill=col,
+                              lwd=lwd, lty=lty))
+  } else if (arrowType == "dot" || arrowType == "odot") {
+    # FIXME: does not scale correctly compared to graphviz. documentation shows
+    # graphviz sets default radius of 2, but does not work here. using 'length'
+    # gets reasonably close results for arrowsize ~= 1
+    dx <- endX - startX
+    dy <- endY - startY
+    length <- sqrt(dx^2 + dy^2) 
+    theta <- atan2(dy, dx)
+    r <- length/2 * arrowsize
+    # FIXME: transparent circle will show edge inside if arrowsize > 1 due to
+    # Rgraphviz not passing through arrow info at layout
+    if (arrowType == "odot") fill <- "transparent" else fill <- col
+    head <- circleGrob(endX - r*cos(theta), endY - r*sin(theta),
+                       r=r, default.units="native", name=name,
+                       gp=gpar(col=col, fill=fill, lwd=lwd, lty=lty))
+    segment <- NULL
+    # as different arrowheads cannot currently be passed through Rgraphviz's
+    # agopen when laying out a line segment connects the edge to the dot head
+    # when the arrowsize is less than the gap 'length'
+    if (arrowsize < 1) {
+      segment <- segmentsGrob(startX, startY,
+                              grobX(name, theta*180/pi),
+                              grobY(name, theta*180/pi),
+                              default.units="native",
+                              gp=gpar(col=col, fill=col,
+                                      lwd=lwd, lty=lty))
+    } 
+    z <- gList(head, segment)
+  } else if (arrowType == "box" || arrowType == "obox") {
+    #FIXME: calculation of length, r, etc has same problems as with "dot" above
+    dx <- endX - startX
+    dy <- endY - startY
+    length <- sqrt(dx^2 + dy^2) 
+    theta <- atan2(dy, dx)
+    r <- length/2 * arrowsize
+    boxvp <- viewport(x=endX - r*cos(theta), y=endY - r*sin(theta),
+                      width=r*2, height=r*2, default.units="native",
+                      angle=theta*180/pi)
+    if (arrowType == "obox") fill <- "transparent" else fill <- col
+    head <- rectGrob(vp=boxvp, gp=gpar(col=col, fill=fill,
+                                       lwd=lwd, lty=lty), name=name)
+    segment <- NULL
+    if (arrowsize < 1) {
+      segment <- segmentsGrob(startX, startY,
+                              grobX(name, theta*180/pi),
+                              grobY(name, theta*180/pi),
+                              default.units="native",
+                              gp=gpar(col=col, fill=col,
+                                      lwd=lwd, lty=lty))
+    } 
+    z <- gList(head, segment)
+  }
+}
